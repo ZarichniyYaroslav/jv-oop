b214cced249ef33aa2d4b27867af205341e56581
jaryk <zarichniyyaroslav@gmail.com>
2026-01-09 18:52:41 +0100

    Added array of Machines in Main method and calles doWork and stopWork methods in loop

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,22 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine[] machines = new Machine[7];
+
+        machines[0] = new Truck();
+        machines[1] = new Bulldozer();
+        machines[2] = new Excavator();
+        machines[3] = new Excavator();
+        machines[4] = new Truck();
+        machines[5] = new Bulldozer();
+        machines[6] = new Truck();
+
+        for (int i = 0; i < machines.length; i++) {
+            machines[i].doWork();
+            machines[i].stopWork();
+        }
+
+    }
 
 }
